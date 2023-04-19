c5fd6b37a89c199d494f5dda2bb7e42eebf984fc
nuuuwan <nuuuwan@gmail.com>
2023-04-19 10:58:16 +0530

    various

--- src/lgn/core/network.py
+++ src/lgn/core/network.py
@@ -2,21 +2,11 @@ import copy
 from functools import cached_property
 
 from gig import Ent
-from utils import List, Log
+from utils import Log
 
 from lgn.utils import distance_matrix, shape_utils
 
 log = Log('network')
-
-
-def point_to_key(point):
-    lng, lat = point
-    return f'{lng:.2f}_{lat:.2f}'
-
-
-def filter_ent(ent):
-    return True
-
 
 def build_node_idx(ent_list):
     node_idx = {}
@@ -36,41 +26,6 @@ def build_node_idx(ent_list):
     return node_idx
 
 
-def build_key_to_ent_set(ent_list):
-    key_to_ent_set = {}
-    for ent in ent_list:
-        raw_geo = ent.get_raw_geo()
-        point_list = List(raw_geo).flatten()
-        key_list = [point_to_key(point) for point in point_list]
-        ent_id = ent.name
-        for key in key_list:
-            if key not in key_to_ent_set:
-                key_to_ent_set[key] = set()
-            key_to_ent_set[key].add(ent_id)
-    return key_to_ent_set
-
-
-def buiid_neighbor_idx(key_to_ent_set):
-    neighbor_idx = {}
-    for key, ent_set in key_to_ent_set.items():
-        if len(ent_set) == 1:
-            continue
-
-        for ent_id in ent_set:
-            if ent_id not in neighbor_idx:
-                neighbor_idx[ent_id] = set()
-            neighbor_idx[ent_id] |= ent_set
-    return neighbor_idx
-
-
-def build_edge_pair_list(neighbor_idx):
-    edge_pair_list = []
-    for id, id_set in neighbor_idx.items():
-        for id2 in id_set:
-            edge_pair_list.append([id, id2])
-    return edge_pair_list
-
-
 class Network:
     def __init__(self, node_idx, edge_pair_list):
         self.node_idx = node_idx
@@ -81,11 +36,8 @@ class Network:
         ent_list = Ent.list_from_type(ent_type)
         if func_filter_ent is not None:
             ent_list = [ent for ent in ent_list if func_filter_ent(ent)]
-
         node_idx = build_node_idx(ent_list)
-        key_to_ent_set = build_key_to_ent_set(ent_list)
-        neighbor_idx = buiid_neighbor_idx(key_to_ent_set)
-        edge_pair_list = build_edge_pair_list(neighbor_idx)
+        edge_pair_list = []
         return Network(node_idx, edge_pair_list)
 
     @cached_property
@@ -116,16 +68,6 @@ class Network:
             neighbor_idx[id1].append(id2)
             neighbor_idx[id2].append(id1)
         return neighbor_idx
-
-    @property
-    def junction_set(self):
-        return set(
-            [
-                id
-                for id, neighbor_list in self.neighbor_idx.items()
-                if len(neighbor_list) > 2
-            ]
-        )
 
     @property
     def distance_matrix(self):
